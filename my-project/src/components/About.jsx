4e576068bf3f65f78bcf345894d8432f61af11c7
Sujit-Thapa <sujitthapa.professiona@gmaill.com>
2025-01-02 20:50:20 +0545

    updated

--- my-project/src/components/About.jsx
+++ my-project/src/components/About.jsx
@@ -6,7 +6,7 @@ const About = () => {
     <section id="about">
       <h1>About Me</h1>
       <p>
-        Hi, I'm [Your Name], a passionate developer. I specialize in creating modern
+        Hi, I'm sujit thapa, a passionate developer. I specialize in creating modern
         and user-friendly websites and applications.
       </p>
     </section>
